e37192fbd1c56babbb7a2fa02fbfed94d44508b3
RenatoDaSilva <renato.katia@gmail.com>
2021-01-15 01:10:17 -0300

    Makes parameters be optional

--- functions/schema.py
+++ functions/schema.py
@@ -5,50 +5,55 @@ from graphene_django import DjangoObjectType
 from .models import Profile
 from django.db.models import Q
 
+
+class ProfileInput(graphene.InputObjectType):
+    email = graphene.String(required=False)
+    fullname = graphene.String(required=False)
+    birthdate = graphene.String(required=False)
+    phones = graphene.List(graphene.String, required=False)
+    preferred_contact = graphene.String(required=False)
+
+
 class setType(DjangoObjectType):
     class Meta:
         model = Profile
 
+
 class setProfile(graphene.Mutation):
     profile = graphene.Field(setType)
-    
-    
+
     class Arguments:
         token = graphene.String()
-        email = graphene.String()
-        fullname = graphene.String()
-        birthdate = graphene.String()
-        phones = graphene.List(graphene.String)
-        preferred_contact = graphene.String()
+        profile_data = ProfileInput(required=True)
 
-    def mutate(self,info,token, email, fullname, birthdate, phones, preferred_contact, *profile):
-        profile = Profile(token=token, 
-        email=email, 
-        fullname=fullname,
-        birthdate=birthdate,
-        phones=phones,
-        preferred_contact=preferred_contact)
+    def mutate(self, info, token, profile_data=None, *profile):
+        profile = Profile(token=token,
+                          email=profile_data.email,
+                          fullname=profile_data.fullname,
+                          birthdate=profile_data.birthdate,
+                          phones=profile_data.phones,
+                          preferred_contact=profile_data.preferred_contact
+                          )
         profile.save()
-        
-        
+
         return setProfile(
             profile=profile)
-           
 
 
 class Mutation(graphene.ObjectType):
     set_profile = setProfile.Field()
 
+
 class Query(graphene.ObjectType):
-    get_profile = graphene.List(setType, 
-    token=graphene.String(),
-    )
-    
-    def resolve_getprofile(self, info, profile,token=None,**kwargs):
+    get_profile = graphene.List(setType,
+                                token=graphene.String(),
+                                )
+
+    def resolve_getprofile(self, info, profile, token=None, profile_data=None, **kwargs):
         if token:
             filter = (
                 Q(profile__icontains=token)
             )
             return Profile.objects.filter(filter)
-        
+
         return Profile.objects.all()
