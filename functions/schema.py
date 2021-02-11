027c4207b49b679de1d40a12dc9c284bfe01dcd3
Renan <renan_naue2@hotmail.com>
2021-02-11 14:59:11 -0300

    add insurance model

--- functions/schema.py
+++ functions/schema.py
@@ -4,61 +4,9 @@ from graphene_django import DjangoObjectType
 
 from .models import *
 from django.db.models import Q
+from .puts import *
 
 from .auth import getCPFFromAuth
-
-
-class ImmovablePropertiesInput(graphene.InputObjectType):
-    profile = graphene.ID()
-    description = graphene.String(required=False)
-    value = graphene.Float(required=False)
-    rented = graphene.Boolean(required=False)
-    funded = graphene.Boolean(required=False)
-    insurance_value = graphene.Float(required=False)
-    insurance_company = graphene.String(required=False)
-
-
-class ImmovablePropertiesOutput(graphene.ObjectType):
-    description = graphene.String(required=False)
-    value = graphene.Float(required=False)
-    rented = graphene.Boolean(required=False)
-    funded = graphene.Boolean(required=False)
-    insurance_value = graphene.Float(required=False)
-    insurance_company = graphene.String(required=False)
-
-
-class ProfileInput(graphene.InputObjectType):
-    email = graphene.String(required=False)
-    fullname = graphene.String(required=False)
-    birthdate = graphene.Date(required=False)
-    preferred_contact = graphene.String(required=False)
-    occupation = graphene.String(required=False)
-    role = graphene.String(required=False)
-    company_name = graphene.String(required=False)
-    business_email = graphene.String(required=False)
-    business_kind = graphene.String(required=False)
-    business_field = graphene.String(required=False)
-    company_has_private_insurance = graphene.Boolean(required=False)
-    income_tax_declaration_type = graphene.String(required=False)
-    social_security_value = graphene.Float(required=False)
-    private_security_your_value = graphene.Float(required=False)
-    private_security_company_value = graphene.Float(required=False)
-    private_security_current_balance = graphene.Float(required=False)
-    monthly_expenses = graphene.Float(required=False)
-    costs_with_dependents = graphene.Float(required=False)
-    how_much_you_save = graphene.Float(required=False)
-    debt_loans = graphene.Float(required=False)
-    partner_in_company = graphene.Float(required=False)
-    health = graphene.String(required=False)
-    plans_and_projects = graphene.String(required=False)
-    current_investment_process = graphene.String(required=False)
-    accepts_info_about_courses = graphene.Boolean(required=False)
-    follow_economic_news = graphene.Boolean(required=False)
-    have_financial_concerns = graphene.String(required=False)
-    additional_info = graphene.String(required=False)
-    phones = graphene.List(graphene.String, required=False)
-    immovable_properties = graphene.List(
-        ImmovablePropertiesInput, required=False)
 
 
 class setType(DjangoObjectType):
@@ -77,6 +25,11 @@ class setType(DjangoObjectType):
         return ImmovableProperties.objects.filter(
             profile__in=str(self.id)).values()
 
+    insurance = graphene.List(InsuranceOutPut)
+
+    def relsove_insurance(self, info):
+        return Insurance.objects.filter(
+            profile__in=str(self.id)).values()
 
 class setProfile(graphene.Mutation):
     profile = graphene.Field(setType)
@@ -89,6 +42,7 @@ class setProfile(graphene.Mutation):
         cpfFromAuth = str(getCPFFromAuth(token))
 
         immovable_properties = profile_data.pop("immovable_properties")
+        insurance = profile_data.pop("insurance")
 
         profile, created = Profile.objects.update_or_create(cpf=cpfFromAuth,
                                                             defaults={
@@ -117,6 +71,19 @@ class setProfile(graphene.Mutation):
                                                            insurance_company=immovable_property['insurance_company'],
                                                            )
                 immovable_properties.save()
+
+        if insurance:
+            Insurance.objects.filter(profile=profile).delete()
+            for insurancy in insurance:
+                insurance = Insurance(profile=profile,
+                                      insurance_type=insurancy['insurance_type'],
+                                      value=insurancy['value'],
+                                      monthly_fee=insurancy['monthly_fee'],
+                                      coverage=insurancy['coverage'],
+                                      company=insuracy['company'],
+                                    
+                                      
+                )
 
 
 class Mutation(graphene.ObjectType):
