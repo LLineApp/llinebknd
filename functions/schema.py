05300977f867cb54ceffd5c527ba72ba6e4d8ddb
Renato da Silva <renato.katia@gmail.com>
2021-02-10 22:58:50 -0300

    Adds a new model immovable properties #11

--- functions/schema.py
+++ functions/schema.py
@@ -2,19 +2,35 @@ import graphene
 
 from graphene_django import DjangoObjectType
 
-from .models import Profile, Phones
+from .models import *
 from django.db.models import Q
 
 from .auth import getCPFFromAuth
 
-import json
+
+class ImmovablePropertiesInput(graphene.InputObjectType):
+    profile = graphene.ID()
+    description = graphene.String(required=False)
+    value = graphene.Float(required=False)
+    rented = graphene.Boolean(required=False)
+    funded = graphene.Boolean(required=False)
+    insurance_value = graphene.Float(required=False)
+    insurance_company = graphene.String(required=False)
+
+
+class ImmovablePropertiesOutput(graphene.ObjectType):
+    description = graphene.String(required=False)
+    value = graphene.Float(required=False)
+    rented = graphene.Boolean(required=False)
+    funded = graphene.Boolean(required=False)
+    insurance_value = graphene.Float(required=False)
+    insurance_company = graphene.String(required=False)
 
 
 class ProfileInput(graphene.InputObjectType):
     email = graphene.String(required=False)
     fullname = graphene.String(required=False)
-    birthdate = graphene.String(required=False)
-    phones = graphene.List(graphene.String, required=False)
+    birthdate = graphene.Date(required=False)
     preferred_contact = graphene.String(required=False)
     occupation = graphene.String(required=False)
     role = graphene.String(required=False)
@@ -22,33 +38,32 @@ class ProfileInput(graphene.InputObjectType):
     business_email = graphene.String(required=False)
     business_kind = graphene.String(required=False)
     business_field = graphene.String(required=False)
-    income_tax_declaration_type = graphene.String(required=False)
     company_has_private_insurance = graphene.Boolean(required=False)
-    social_security_value = graphene.Int(required=False)
-    private_security_your_value = graphene.Int(required=False)
-    private_security_company_value = graphene.Int(required=False)
-    private_security_current_balance = graphene.Int(required=False)
-    monthly_expenses = graphene.Int(required=False)
-    costs_with_dependents = graphene.Int(required=False)
-    how_much_you_save = graphene.Int(required=False)
-    debt_loans = graphene.Int(required=False)
-    partner_in_company = graphene.Int(required=False)
+    income_tax_declaration_type = graphene.String(required=False)
+    social_security_value = graphene.Float(required=False)
+    private_security_your_value = graphene.Float(required=False)
+    private_security_company_value = graphene.Float(required=False)
+    private_security_current_balance = graphene.Float(required=False)
+    monthly_expenses = graphene.Float(required=False)
+    costs_with_dependents = graphene.Float(required=False)
+    how_much_you_save = graphene.Float(required=False)
+    debt_loans = graphene.Float(required=False)
+    partner_in_company = graphene.Float(required=False)
     health = graphene.String(required=False)
     plans_and_projects = graphene.String(required=False)
     current_investment_process = graphene.String(required=False)
     accepts_info_about_courses = graphene.Boolean(required=False)
     follow_economic_news = graphene.Boolean(required=False)
     have_financial_concerns = graphene.String(required=False)
-    addicional_info = graphene.String(required=False)
+    additional_info = graphene.String(required=False)
+    phones = graphene.List(graphene.String, required=False)
+    immovable_properties = graphene.List(
+        ImmovablePropertiesInput, required=False)
 
 
-
-
-    
 class setType(DjangoObjectType):
     class Meta:
         model = Profile
-
 
     phones = graphene.List(graphene.String)
 
@@ -56,6 +71,11 @@ class setType(DjangoObjectType):
         return Phones.objects.filter(
             profile__in=str(self.id)).values_list('phone', flat=True)
 
+    immovable_properties = graphene.List(ImmovablePropertiesOutput)
+
+    def resolve_immovable_properties(self, info):
+        return ImmovableProperties.objects.filter(
+            profile__in=str(self.id)).values()
 
 
 class setProfile(graphene.Mutation):
@@ -67,6 +87,9 @@ class setProfile(graphene.Mutation):
 
     def mutate(self, info, token, profile_data=None):
         cpfFromAuth = str(getCPFFromAuth(token))
+
+        immovable_properties = profile_data.pop("immovable_properties")
+
         profile, created = Profile.objects.update_or_create(cpf=cpfFromAuth,
                                                             defaults={
                                                                 **profile_data}
@@ -82,8 +105,18 @@ class setProfile(graphene.Mutation):
                                 )
                 phones.save()
 
-        return setProfile(
-            profile=profile)
+        if immovable_properties:
+            ImmovableProperties.objects.filter(profile=profile).delete()
+            for immovable_property in immovable_properties:
+                immovable_properties = ImmovableProperties(profile=profile,
+                                                           description=immovable_property['description'],
+                                                           value=immovable_property['value'],
+                                                           rented=immovable_property['rented'],
+                                                           funded=immovable_property['funded'],
+                                                           insurance_value=immovable_property['insurance_value'],
+                                                           insurance_company=immovable_property['insurance_company'],
+                                                           )
+                immovable_properties.save()
 
 
 class Mutation(graphene.ObjectType):
