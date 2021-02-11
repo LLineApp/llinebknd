f312a6649e6255f2cc88668aa6cead3c22abc74c
Renan <renan_naue2@hotmail.com>
2021-02-11 20:47:56 -0300

    fix some issues

--- functions/inputs.py
+++ functions/inputs.py
@@ -2,6 +2,14 @@ import graphene
 from graphene_django import DjangoObjectType
 
 
+class InsuranceInput(graphene.InputObjectType):
+    profile = graphene.ID()
+    insurance_type = graphene.String(required=False)
+    value = graphene.Float(required=False)
+    monthly_fee = graphene.Boolean(required=False)
+    coverage = graphene.Float(required=False)
+    company = graphene.String(required=False)
+
 
 class InvestorExperienceInput(graphene.InputObjectType):
     kind = graphene.String(required=False)
@@ -50,14 +58,8 @@ class ProfileInput(graphene.InputObjectType):
     phones = graphene.List(graphene.String, required=False)
     immovable_properties = graphene.List(ImmovablePropertiesInput, required=False)
     investor_experience = graphene.List(InvestorExperienceInput, required=False)
-    insurance = graphene.List(required=False)
+    insurance = graphene.List(InsuranceInput, required=False)
 
 
 
-class InsuranceInput(graphene.InputObjectType):
-    profile = graphene.ID()
-    insurance_type = graphene.String(required=False)
-    value = graphene.Float(required=False)
-    monthly_fee = graphene.Boolean(required=False)
-    coverage = graphene.Float(required=False)
-    company = graphene.String(required=False)
+
