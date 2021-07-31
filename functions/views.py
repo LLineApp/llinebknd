1f0768ce2ee1ba11355726e63fdc4df3b554d132
Renato da Silva <renato.katia@gmail.com>
2021-07-31 19:11:48 -0300

    little fixes

--- functions/views.py
+++ functions/views.py
@@ -4,6 +4,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def get_clients_portfolio_from_advisor_help(request):
-    print(os.path.join(BASE_DIR, 'llinebknd/templates/help/getClientsPortfolioFromAdvisor.html'))
     return render(request, 'help/getClientsPortfolioFromAdvisor.html')
-#    return render(request, os.path.join(BASE_DIR, 'llinebknd/templates/help/getClientsPortfolioFromAdvisor.html'))
