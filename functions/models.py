5cdd2274ef3fae7a91b428fc938f08c24d4a86dd
Renan <renan_naue2@hotmail.com>
2021-08-17 15:38:21 -0300

    add active field to financial advisors model

--- functions/models.py
+++ functions/models.py
@@ -7,6 +7,7 @@ class FinancialAdvisors(models.Model):
     register = models.TextField(blank=True, null=True)
     company = models.TextField(blank=True, null=True)
     cpf = models.TextField(blank=True, null=True)
+    active = models.NullBooleanField(blank=True, null=True)
 
 
 class Profile(models.Model):
