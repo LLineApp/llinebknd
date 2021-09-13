3f1443c6f51963d42a11b5bda9afb90f8bb7e9c6
Renato da Silva <renato.katia@gmail.com>
2021-09-13 20:16:45 -0300

    Adjusts lifeline to bring values year by year #148 (#149)

--- llinebknd/functions/life_line.py
+++ llinebknd/functions/life_line.py
@@ -2,36 +2,37 @@ import datetime
 from .math import compoundInterest, convertYearInterestToMonthInterest
 from .date_utils import monthsBetween
 
+
 def get_master_line(targets):
-    periods = []
-    amount = []
+    master_line = {}
     if targets.exists():
-        start_year = targets[0].date.year
-        end_year = targets[0].year_to_start_withdraw
-        start_value =targets[0].present_value
-        end_value = 0
+        present_value = 0
         index = 1
+        master_line[targets[0].date.year] = targets[0].present_value
         for target in targets:
-            present_value = end_value + target.present_value
+            present_value += target.present_value
 
             try:
                 next_date = targets[index].date
-            except:    
-                next_date = datetime.datetime(target.year_to_start_withdraw-1, 12, 31)
+            except:
+                next_date = datetime.date(target.year_to_start_withdraw, 1, 1)
             index += 1
-
-            invested_time = monthsBetween(target.date, next_date)
 
             numeric_interest = target.suitability.interest/100
 
-            end_value = compoundInterest(
-                present_value, 
-                convertYearInterestToMonthInterest(numeric_interest),
-                invested_time, 
-                target.monthly_investment)
+            for year in range(target.date.year, next_date.year + 1):
+                next_year = year + 1
+                start_date = max([target.date, datetime.date(year, 1, 1)])
+                end_date = min([next_date, datetime.date(next_year, 1, 1)])
 
-        periods = [start_year, end_year]
-        amount = [start_value, round(end_value, 2)]
+                if monthsBetween(start_date, end_date) > 0:
+                    present_value = compoundInterest(
+                        present_value,
+                        convertYearInterestToMonthInterest(numeric_interest),
+                        monthsBetween(start_date, end_date),
+                        target.monthly_investment)
 
-    return periods, amount
+                    master_line.setdefault(next_year, 0)
+                    master_line[next_year] = round(present_value, 2)
 
+    return master_line.keys(), master_line.values()
